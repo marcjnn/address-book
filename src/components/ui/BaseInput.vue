91946ee75d3f4356cc21cbfab9b880f6b22acc0b
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-05-08 06:35:44 +0200

    add BaseInput

---
+++ src/components/ui/BaseInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <label v-if="label" class="field__label">{{ label }}</label>
+  <input
+    :value="modelValue"
+    v-bind="$attrs"
+    :placeholder="placeholder"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="form__field"
+  />
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form__field {
+  padding: 6px;
+  border: 1px solid grey;
+  display: block;
+  width: 80%;
+}
+
+.field__label {
+  display: block;
+}
+</style>
