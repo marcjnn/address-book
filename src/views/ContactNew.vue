7a9db46b12e01da1be4faf5ebee712ed38230724
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-05-08 14:45:57 +0200

    remove commented code from ContactNew

--- src/views/ContactNew.vue
+++ src/views/ContactNew.vue
@@ -4,17 +4,10 @@
 </template>
 
 <script>
-// import UniqueID from "@/features/UniqueID";
 import ContactEdit from "@/components/contact/ContactEdit.vue";
 export default {
   name: "ContactNew",
   components: { ContactEdit },
-  // setup() {
-  //   const uuid = UniqueID().getID();
-  //   return {
-  //     uuid,
-  //   };
-  // },
 };
 </script>
 
