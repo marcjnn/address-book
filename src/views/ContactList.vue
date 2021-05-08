25e611839bea92c44620d11b29dc204c61d669b0
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-05-08 06:51:19 +0200

    update ContactList component

--- src/views/ContactList.vue
+++ src/views/ContactList.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <ul class="contact__list">
+    <li v-for="(contact, index) in contactsSorted" :key="index">
+      <ContactCard :contact="contact" />
+    </li>
+  </ul>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ContactCard from "@/components/ContactCard.vue";
 
 export default {
-  name: "Home",
+  name: "ContactList",
   components: {
-    HelloWorld,
+    ContactCard,
+  },
+  data() {
+    return {
+      contacts: [
+        {
+          id: 0,
+          firstName: "Adam",
+          lastName: "Małysz",
+          email: "adam@mailme.com",
+          country: "Poland",
+        },
+        {
+          id: 1,
+          firstName: "Matti",
+          lastName: "Hautamäki",
+          email: "matti@mailme.com",
+          country: "Finland",
+        },
+        {
+          id: 2,
+          firstName: "Simon",
+          lastName: "Ammann",
+          email: "simi@mailme.com",
+          country: "Switzerland",
+        },
+        {
+          id: 3,
+          firstName: "Martin",
+          lastName: "Schmidt",
+          email: "martin@mailme.com",
+          country: "Germany",
+        },
+      ],
+    };
+  },
+  mounted() {
+    // if (localStorage.contacts) {
+    //   this.contacts = localStorage.contacts
+    // }
+  },
+  // watch: {
+  //   contacts(newContact) {
+  //     localStorage.contacts.push(newContact)
+  //   },
+  // },
+  computed: {
+    // contacts() {
+    //   return store.getters.getContacts;
+    // },
+    contactsSorted() {
+      let newArray = [...this.contacts];
+      return newArray.sort(this.sortByLastName);
+    },
+  },
+  methods: {
+    sortByLastName(a, b) {
+      const nameA = a.lastName.toLowerCase();
+      const nameB = b.lastName.toLowerCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    },
   },
 };
 </script>
+<style scoped>
+.contact__list {
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  /* align-items: center; */
+}
+</style>
